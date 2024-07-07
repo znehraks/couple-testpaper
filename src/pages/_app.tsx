0814495db634c55ee3e6dbb94afc3a119f204b82
znehraks <znehraks@gmail.com>
2024-07-08 01:39:49 +0900

    feat: jotai, react-query devtools 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,19 @@ import '@/styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import { Provider } from 'jotai';
-import { ChakraProvider } from '@chakra-ui/react';
+import { DevTools } from 'jotai-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import 'jotai-devtools/styles.css';
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider>
-      <ChakraProvider>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        {process.env.NODE_ENV === 'development' && <DevTools />}
+        {process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </Provider>
   );
 }
