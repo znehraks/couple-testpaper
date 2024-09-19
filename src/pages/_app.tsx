173e5a20f13b12c1867316f077f6314263684aca
znehraks <znehraks@gmail.com>
2024-09-20 02:27:01 +0900

    ux: fontfaceobserver로 폰트가 로드되지않으면 화면을 띄우지 않음

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,35 @@ import { DevTools } from 'jotai-devtools';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import 'jotai-devtools/styles.css';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import FontFaceObserver from 'fontfaceobserver';
+import { Spinner } from '@/components/common/Spinner';
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const fonts = [
+      new FontFaceObserver('NanumBaeEunHyeCe'),
+      new FontFaceObserver('NanumSquareL'),
+      new FontFaceObserver('NanumSquareB'),
+      new FontFaceObserver('NanumSquareEB'),
+      new FontFaceObserver('NanumSquareR'),
+      new FontFaceObserver('NanumMyeongjo'),
+    ];
+
+    Promise.all(fonts.map((font) => font.load()))
+      .then(() => {
+        setFontsLoaded(true);
+      })
+      .catch((err) => {
+        console.error('Some fonts were not loaded', err);
+        setFontsLoaded(true); // 폰트 로딩 실패시에도 앱을 표시
+      });
+  }, []);
+
+  if (!fontsLoaded) return <Spinner />;
   return (
     <>
       <Head>
